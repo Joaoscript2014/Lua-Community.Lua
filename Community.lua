e1556de38b2cce293837d2337d457d0693b1b41a
Joaoscript2014 <joaovitorbritoramos27.04.2003@gmail.com>
2025-07-03 14:08:33 -0300

    Community.lua

---
+++ Community.lua
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <style>
+    body {
+      margin: 0;
+      height: 100vh;
+      background: linear-gradient(to bottom, #000000, #800080);
+    }
+  </style>
+</head>
+<body>
+</body>
+</html>
